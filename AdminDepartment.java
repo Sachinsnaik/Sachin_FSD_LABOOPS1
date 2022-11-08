528b287e80b583e51dd7d5e224f9f371bfd116d4
Sachinsnaik <114140719+Sachinsnaik@users.noreply.github.com>
2022-11-08 23:05:40 +0530

    Add files via upload

---
+++ AdminDepartment.java
@@ -0,0 +1,17 @@
+package Services;
+
+public class AdminDepartment extends SuperDepartment { 
+	
+	public void departmantName() { 
+		System.out.println("Welcome to AdminDepartment");
+	}
+
+	public void getTodaysWork() {
+		System.out.println("Complete your document submission");
+	}
+
+	public void getWorkDeadline() {
+		System.out.println("Complete by EOD");
+	}
+
+}
---
+++ SuperDepartment.java
@@ -0,0 +1,21 @@
+package Services;
+
+public class SuperDepartment { 
+	
+	public void departmantName() { 
+		System.out.println("Welcome to SuperDepartment");
+	}
+
+	public void getTodaysWork() {
+		System.out.println("No work as of now");
+	}
+
+	public void getWorkDeadline() {
+		System.out.println("Nil");
+	}
+
+	public void isTodayAHoliday() {
+		System.out.println("Today is not a holiday");
+	}
+
+}
